16fec689004f863b90a4a1ddfa6bb0144f70bae8
trickypr <trickypr@users.noreply.github.com>
2020-07-14 19:05:19 +1000

    Small tweeks to installer

--- installer/installer.sh
+++ installer/installer.sh
@@ -11,15 +11,15 @@ Cya='\e[0;36m';     BCya='\e[1;36m';    UCya='\e[4;36m';    ICya='\e[0;96m';    
 Whi='\e[0;37m';     BWhi='\e[1;37m';    UWhi='\e[4;37m';    IWhi='\e[0;97m';    BIWhi='\e[1;97m';   On_Whi='\e[47m';    On_IWhi='\e[0;107m';
 
 info () {
-  echo Cya $1 RCol
+  echo $Cya$1$RCol
 }
 
 info 'Reached the installer repo and the main installer scripts'
 echo
 
 info 'Updating apt'
-sudo apt update && sudo apt upgrade
-sudo apt install awesome lightdm qterminal mate-polkit
+sudo apt update 
+sudo apt install -y awesome lightdm qterminal mate-polkit
 
 echo
 echo
