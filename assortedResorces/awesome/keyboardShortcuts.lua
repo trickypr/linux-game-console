9c5493ae6ec41652aeeb07267756e9e791a5ab10
trickypr <trickypr@users.noreply.github.com>
2020-07-15 12:14:21 +1000

    Remove useless keyboard shortcuts

--- assortedResorces/awesome/keyboardShortcuts.lua
+++ assortedResorces/awesome/keyboardShortcuts.lua
@@ -39,24 +39,6 @@ keyboardShortcuts.globalKeys = gears.table.join(
     awful.tag.viewnext,
     {description = "view next", group = "tag"}
   ),
-  
-  -- Switch between active windows
-  awful.key(
-    { "Alt",}, 
-    "Tab",
-    function ()
-        awful.client.focus.byidx( 1)
-    end,
-    {description = "focus next by index", group = "client"}
-  ),
-  awful.key(
-    { "Alt",}, 
-    "Tab",
-    function ()
-        awful.client.focus.byidx(-1)
-    end,
-    { description = "focus previous by index", group = "client" }
-  ),
 
   -- Swap between layouts
   awful.key(
